7cd143e9d099d5cf26ee4e78d28f5b7a0a92901d
sameer3841 <81244405+sameer3841@users.noreply.github.com>
2024-02-26 12:49:28 -0500

    finish commenting my files

--- FoodOrdering/src/components/Themed.tsx
+++ FoodOrdering/src/components/Themed.tsx
@@ -4,18 +4,24 @@
  */
 
 import { Text as DefaultText, View as DefaultView } from 'react-native';
-
 import Colors from '../../src/constants/Colors';
 import { useColorScheme } from './useColorScheme';
 
+// creates the theme  based on the color scheme (light or dark)
 type ThemeProps = {
   lightColor?: string;
   darkColor?: string;
 };
-
+// creates  a text component that will be either light or dark depending on the user's color scheme preference
 export type TextProps = ThemeProps & DefaultText['props'];
+// creates  a view component that uses the correct colors for the current color scheme
 export type ViewProps = ThemeProps & DefaultView['props'];
 
+/**
+ * a reusable function to get the text style props based on the color scheme
+ * @param {object} props - the props passed into the component
+ * @returns {{color: string}} an object with the color property set to the appropriate value
+ **/
 export function useThemeColor(
   props: { light?: string; dark?: string },
   colorName: keyof typeof Colors.light & keyof typeof Colors.dark
@@ -30,6 +36,10 @@ export function useThemeColor(
   }
 }
 
+/**
+ * a  wrapper around react native's default `View` component but it will 
+ * use the color defined in the current color scheme
+ **/
 export function Text(props: TextProps) {
   const { style, lightColor, darkColor, ...otherProps } = props;
   const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
@@ -37,6 +47,10 @@ export function Text(props: TextProps) {
   return <DefaultText style={[{ color }, style]} {...otherProps} />;
 }
 
+/**
+ * A wrapper around react-native's 
+ * `View` component that gets its color from the current color scheme
+ */
 export function View(props: ViewProps) {
   const { style, lightColor, darkColor, ...otherProps } = props;
   const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
